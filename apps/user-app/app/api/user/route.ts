b75a356e4555ce152730d28343759da35446f70e
Kartik Vashishtha <kartik2005vashishtha@gmail.com>
2025-07-06 19:39:01 +0530

    changed_orm

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -1,16 +1,17 @@
-import { PrismaClient } from "@repo/db/client";
+
 import { NextResponse } from "next/server";
+import { PrismaClient } from "../../../../../packages/db/generated/prisma";
 
 const prisma = new PrismaClient() ;
 
 export async function GET() {
-    await prisma.$connect() ;
     const user = await prisma.user.create({
         data : {
             name : "John Doe",
             email : "jhon@123"
         }
     }) ;
+    
     return NextResponse.json({
         message: "User created successfully",
         user : user
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,2 +1,1 @@
-
-export * from './generated/prisma';
+export * from "@prisma/client"
